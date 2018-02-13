08ec87953645508a8e83ebfbd1277b59275be9e6
thobalose <lose.thoba@gmail.com>
2018-02-13 15:33:51 +0200

    es test

---
+++ test/test_elasticsearch.py
@@ -0,0 +1,14 @@
+import os
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+
+es_client = Elasticsearch(hosts=[
+    {'host': os.environ.get("DB", "localhost")},
+])
+
+
+def test_es_search():
+    search = Search(using=es_client, index="_all") \
+        .query("multi_match", query="katg", fields=['name', 'uniquename'])
+    response = search.execute()
+    assert response.hits.total is not None
