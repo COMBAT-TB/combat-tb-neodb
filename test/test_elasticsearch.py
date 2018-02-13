ad70e0ee807d2edb2067377e11daf7e08d47a1c5
thobalose <lose.thoba@gmail.com>
2018-02-13 16:05:22 +0200

    bug

--- test/test_elasticsearch.py
+++ test/test_elasticsearch.py
@@ -11,4 +11,4 @@ def test_es_search():
     search = Search(using=es_client, index="_all") \
         .query("multi_match", query="katg", fields=['name', 'uniquename'])
     response = search.execute()
-    assert response.hits.total is not None
+    assert response.hits.total == 2
